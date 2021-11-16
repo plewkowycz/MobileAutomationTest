bf64da56b5e39db46d951c6443449ae91b1f818a
Liwyj <piotr.lewkowycz@gmail.com>
2021-11-16 20:56:20 +0100

    wip

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,14 +1,57 @@
-describe('My Login application', () => {
-    it('should login with valid credentials', async () => {
-        await browser.url(`https://the-internet.herokuapp.com/login`);
+var expect = require('chai').expect
 
-        await $('#username').setValue('tomsmith');
-        await $('#password').setValue('SuperSecretPassword!');
-        await $('button[type="submit"]').click();
+beforeEach(() => {
+ driver.launchApp()
+})
 
-        await expect($('#flash')).toBeExisting();
-        await expect($('#flash')).toHaveTextContaining(
-            'You logged into a secure area!');
-    });
-});
+afterEach(() => {
+ driver.closeApp()
+})
 
+describe('Verify Login Scenarios on Facebook React Native Mobile App', () => {
+ it('User should be able to login using valid credentials to Facebook Mobile App', () => {   
+   $(`~Username`).waitForDisplayed(20000)
+   $(`~Username`).setValue('Valid-Email')
+   $(`~Password`).waitForDisplayed(20000)
+   $(`~Password`).setValue('Valid-Password')
+   $('~Log In').click()
+   browser.pause(10000)
+ })
+
+ it('User should not be able to login with invalid credentials to Facebook Mobile App', () => {
+   $(`~Username`).waitForDisplayed(20000)
+   $(`~Username`).setValue('Invalid-Email')
+   $(`~Password`).waitForDisplayed(20000)
+   $(`~Password`).setValue('Invalid-Password')   
+   $('~Log In').click()
+   $(
+       '//android.widget.TextView[@resource-id="com.facebook.katana:id/(name removed)"]'
+     )
+     .waitForDisplayed(11000)
+   const status = $(
+     '//android.widget.TextView[@resource-id="com.facebook.katana:id/(name removed)"]'
+   ).getText()
+   expect(status).to.equal(
+     `You Can't Use This Feature Right Now`     
+   )
+ })
+
+ it('Test Case should Fail Because of Invalid Element', () => {
+   $(`~Username`).waitForDisplayed(20000)
+   $(`~Username`).setValue('Invalid-Email')
+   $(`~Password`).waitForDisplayed(20000)
+   $(`~Password`).setValue('Invalid-Pasword')   
+   $('~Log In').click()
+   $(
+       '//android.widget.TextView[@resource-id="com.facebook.katana:id/(name removed)"'
+     )
+     .waitForDisplayed(11000)
+   const status = $(
+     '//android.widget.TextView[@resource-id="com.facebook.katana"'
+   ).getText()
+   expect(status).to.equal(
+     `You Can't Use This Feature Right Now`     
+   )
+ })
+
+})
