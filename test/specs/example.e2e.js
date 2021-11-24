9f3b9b934c9890504c47d14c3127187f08344de5
Liwyj <piotr.lewkowycz@gmail.com>
2021-11-24 22:12:16 +0100

    wip

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -9,13 +9,33 @@ afterEach(() => {
 })
 
 describe('Verify Login Scenarios on Saxobank React Native Mobile App', () => {
- it('User should be able to login using valid credentials to Saxobank Mobile App', () => {   
+ it('User should not be able to login using wrong credentials to Saxobank Mobile App', () => {   
    $('//android.widget.EditText[@resource-id="field_userid"]').waitForDisplayed(20000)
    $('//android.widget.EditText[@resource-id="field_userid"]').setValue('Valid-Email')
    $('//android.widget.EditText[@resource-id="field_password"]').waitForDisplayed(20000)
    $('//android.widget.EditText[@resource-id="field_password"]').setValue('Valid-Password')
    $('//android.widget.Button[@resource-id="button_login"]').click()
-   browser.pause(10000)
+
+   let elem = $('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[2]/android.view.View[6]/android.widget.TextView')
+
+   expect(elem).to.equal(elem)
+
  })
+
+ it('User should be able to login using valid credentials to Saxobank Mobile App', () => {   
+  $('//android.widget.EditText[@resource-id="field_userid"]').waitForDisplayed(20000)
+  $('//android.widget.EditText[@resource-id="field_userid"]').setValue('XXXX')
+  $('//android.widget.EditText[@resource-id="field_password"]').waitForDisplayed(20000)
+  $('//android.widget.EditText[@resource-id="field_password"]').setValue('XXXX')
+  $('//android.widget.Button[@resource-id="button_login"]').click()
+  $('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View[5]').click()
+  $('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View/android.view.View[1]/android.view.View/android.view.View[2]/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[8]/android.view.View/android.view.View').click()
+  $('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View[2]/android.view.View[2]/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View[2]/android.widget.Button').click()
+  
+  let elem =  $('//android.widget.EditText[@resource-id="field_userid"]')
+  
+  expect(elem).to.equal(elem)
+ 
+})
 
 })
